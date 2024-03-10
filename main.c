9fca466533af476698ece0101fd6e2717101821b
KevinZheng0701 <kevinzheng700@gmail.com>
2024-03-09 19:46:22 -0500

    Finish ske encrypt and decrpyt with buffers only.

--- main.c
+++ main.c
@@ -1,51 +1,121 @@
+/* test code for SKE */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "rsa.h"
+#include <unistd.h>
 
-int main()
+#include "ske.h"
+#include "prf.h"
+
+/* turn this on to print more stuff. */
+#define VDEBUG 0
+/* turn this on for randomized tests. */
+#define RANDKEY 0
+
+/* encrypt / decrypt some strings, and make sure
+ * this composition is the identity */
+int soundCheck(char *message, SKE_KEY *K, unsigned char *IV, unsigned char errMask)
 {
-    // Generate RSA keys
-    size_t keyBits = 1024; // Example key size
-    RSA_KEY K;
-    rsa_keyGen(keyBits, &K);
-
-    // Print generated keys
-    gmp_printf("p: %Zd\n", K.p);
-    gmp_printf("q: %Zd\n", K.q);
-    gmp_printf("n: %Zd\n", K.n);
-    gmp_printf("e: %Zd\n", K.e);
-    gmp_printf("d: %Zd\n", K.d);
-    // Original message
-    unsigned char *original_message = (unsigned char *)"Hello how are you today. Password 123";
-    gmp_printf("Original message: %s\n", original_message);
-    size_t original_message_len = strlen((const char *)original_message);
-    // Encrypt message
-    size_t encrypted_message_len = (keyBits / 8) + 1; // Adjust length for padding
-    unsigned char *encrypted_message = malloc(encrypted_message_len);
-    size_t encrypted_bytes = rsa_encrypt(encrypted_message, original_message, original_message_len, &K);
-    gmp_printf("Encrypted message: ");
-    for (size_t i = 0; i < encrypted_bytes; i++)
+    size_t len = strlen(message) + 1; /* +1 to include null char */
+    size_t ctLen = ske_getOutputLen(len);
+    int rcode = 1;
+    unsigned char *ct = malloc(ctLen);
+    ske_encrypt(ct, (unsigned char *)message, len, K, IV);
+#if VDEBUG
+    size_t i;
+    for (i = 0; i < ctLen; i++)
     {
-        gmp_printf("%02x", encrypted_message[i]);
+        fprintf(stderr, "%02x", ct[i]);
     }
-    gmp_printf("\n");
-    // Decrypt message
-    unsigned char *decrypted_message = malloc(original_message_len);
-    size_t decrypted_bytes = rsa_decrypt(decrypted_message, encrypted_message, encrypted_bytes, &K);
-    // Print results
-    gmp_printf("Decrypted message: %s\n", decrypted_message);
+    fprintf(stderr, "\n");
+#endif
+    /* now try to decrypt it. */
+    char *pt = malloc(len);
+    /* tweak ct and see what happens: */
+    ct[rand() % ctLen] ^= errMask;
+    size_t r = ske_decrypt((unsigned char *)pt, ct, ctLen, K);
+    if (r == -1)
+    {
+#if VDEBUG
+        fprintf(stderr, "Invalid ciphertext x_x\n");
+#endif
+        rcode = -1;
+        goto end;
+    }
+#if VDEBUG
+    fprintf(stderr, "%s\n", pt);
+#endif
+    rcode = (strcmp(pt, message)) ? 1 : 0;
 
-    // Free memory
-    free(encrypted_message);
-    free(decrypted_message);
-    mpz_clear(K.p);
-    mpz_clear(K.q);
-    mpz_clear(K.n);
-    mpz_clear(K.e);
-    mpz_clear(K.d);
+end:
+    free(ct);
+    free(pt);
+    return rcode;
+}
+
+int main(int argc, char *argv[])
+{
+    fprintf(stderr, "testing ske...\n");
+    char *pass, *fail;
+    if (isatty(fileno(stdout)))
+    {
+        pass = "\033[32mpassed\033[0m";
+        fail = "\033[31mfailed\033[0m";
+    }
+    else
+    {
+        pass = "passed";
+        fail = "failed";
+    }
+    int rcode;
+    char *strings[8] = {
+        "this is a test",
+        "this is a longer test",
+        "another even longer test.........",
+        "asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf",
+        "909090898989787878676767565656454545343434232332121212abbcccddddeeeee",
+        "XXXXXXXXXXXXXXXXXXX",
+        "88888888888888888888888888888888888888",
+        "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH",
+    };
+    size_t i;
+    unsigned char IV[16];
+    for (i = 0; i < 16; i++)
+        IV[i] = i;
+#if RANDKEY
+    setSeed(0, 0);
+#else
+    unsigned char seed[32];
+    for (i = 0; i < 32; i++)
+        seed[i] = (i * 3 + 53) & 0xff;
+    setSeed(seed, 32);
+#endif
+    SKE_KEY K;
+    ske_keyGen(&K, 0, 0);
+
+    if (argc == 4)
+    {
+        ske_encrypt_file(argv[2], argv[1], &K, IV, 0);
+        ske_decrypt_file(argv[3], argv[2], &K, 0);
+        return 0;
+    }
+
+    for (i = 0; i < 8; i++)
+    {
+        rcode = soundCheck(strings[i], &K, IV, 0);
+        fprintf(stderr, "test[%02lu] %s\n", i, rcode ? fail : pass);
+    }
+    /* now with some errors: */
+    for (i = 0; i < 8; i++)
+    {
+        rcode = soundCheck(strings[i], &K, IV, i + 1);
+        fprintf(stderr, "test[%02lu] %s\n", i + 8, (rcode != -1) ? fail : pass);
+    }
 
     return 0;
 }
 
-// gcc -o test_rsa main.c rsa.c prf.c -I/opt/homebrew/Cellar/gmp/6.3.0/include -L/opt/homebrew/Cellar/gmp/6.3.0/lib -lgmp -I/opt/homebrew/Cellar/gmp/6.3.0/include -L/opt/homebrew/Cellar/gmp/6.3.0/lib -lgmp -I/opt/homebrew/opt/openssl@3/include -L/opt/homebrew/opt/openssl@3/lib -lssl -lcrypto
+// RSA gcc -o test_rsa main.c rsa.c prf.c -I/opt/homebrew/Cellar/gmp/6.3.0/include -L/opt/homebrew/Cellar/gmp/6.3.0/lib -lgmp -I/opt/homebrew/opt/openssl@3/include -L/opt/homebrew/opt/openssl@3/lib -lssl -lcrypto
+
+// SKE gcc -o test_ske main.c ske.c prf.c -I/opt/homebrew/Cellar/gmp/6.3.0/include -L/opt/homebrew/Cellar/gmp/6.3.0/lib -lgmp -I/opt/homebrew/opt/openssl@3/include -L/opt/homebrew/opt/openssl@3/lib -lssl -lcrypto
--- ske.c
+++ ske.c
@@ -12,7 +12,7 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #ifdef LINUX
-#define MMAP_SEQ MAP_PRIVATE|MAP_POPULATE
+#define MMAP_SEQ MAP_PRIVATE | MAP_POPULATE
 #else
 #define MMAP_SEQ MAP_PRIVATE
 #endif
@@ -31,43 +31,108 @@
 /* need to make sure KDF is orthogonal to other hash functions, like
  * the one used in the KDF, so we use hmac with a key. */
 
-int ske_keyGen(SKE_KEY* K, unsigned char* entropy, size_t entLen)
+int ske_keyGen(SKE_KEY *K, unsigned char *entropy, size_t entLen)
 {
 	/* TODO: write this.  If entropy is given, apply a KDF to it to get
 	 * the keys (something like HMAC-SHA512 with KDF_KEY will work).
 	 * If entropy is null, just get a random key (you can use the PRF). */
+	if (entropy != NULL) // entropy is given
+	{
+		// generate aeskey
+		HMAC(EVP_sha512(), KDF_KEY, strlen(KDF_KEY), entropy, entLen, K->aesKey, NULL);
+		// generate hmackey
+		HMAC(EVP_sha512(), KDF_KEY, strlen(KDF_KEY), entropy, entLen, K->hmacKey, NULL);
+	}
+	else // entropy is not given
+	{
+		// generate random keys
+		randBytes(K->aesKey, KLEN_SKE);
+		randBytes(K->hmacKey, KLEN_SKE);
+	}
 	return 0;
 }
 size_t ske_getOutputLen(size_t inputLen)
 {
 	return AES_BLOCK_SIZE + inputLen + HM_LEN;
 }
-size_t ske_encrypt(unsigned char* outBuf, unsigned char* inBuf, size_t len,
-		SKE_KEY* K, unsigned char* IV)
+size_t ske_encrypt(unsigned char *outBuf, unsigned char *inBuf, size_t len,
+				   SKE_KEY *K, unsigned char *IV)
 {
 	/* TODO: finish writing this.  Look at ctr_example() in aes-example.c
 	 * for a hint.  Also, be sure to setup a random IV if none was given.
 	 * You can assume outBuf has enough space for the result. */
-	return 0; /* TODO: should return number of bytes written, which
-	             hopefully matches ske_getOutputLen(...). */
+	// generate random IV if none is given
+	if (IV == NULL)
+		randBytes(IV, 16);
+	// encrypt the plaintext and get the ciphertext
+	EVP_CIPHER_CTX *ciphertext = EVP_CIPHER_CTX_new();
+	EVP_EncryptInit_ex(ciphertext, EVP_aes_256_ctr(), 0, K->aesKey, IV);
+	int bytesWritten;
+	EVP_EncryptUpdate(ciphertext, outBuf, &bytesWritten, (unsigned char *)inBuf, len);
+	EVP_CIPHER_CTX_free(ciphertext);
+	// Set up the concanentation of IV and ciphertext
+	unsigned int HMACLength = 32;
+	unsigned char *IVC = malloc(16 + bytesWritten);
+	memcpy(IVC, IV, 16);
+	memcpy(IVC + 16, outBuf, bytesWritten);
+	// Do HMAC(IV|C)
+	unsigned char *hmac = malloc(HMACLength);
+	HMAC(EVP_sha256(), K->hmacKey, HMACLength, IVC, 16 + bytesWritten, hmac, NULL);
+	// Form the ciphertext from the three components, IV, C, and HMAC(IV|C) and write to buffer
+	size_t ciphertextLength = bytesWritten + HMACLength + 16;
+	memcpy(outBuf, IVC, 16 + bytesWritten);
+	memcpy(outBuf + 16 + bytesWritten, hmac, HMACLength);
+	// free up memory
+	free(IVC);
+	free(hmac);
+	return ciphertextLength; /* TODO: should return number of bytes written, which
+									 hopefully matches ske_getOutputLen(...). */
 }
-size_t ske_encrypt_file(const char* fnout, const char* fnin,
-		SKE_KEY* K, unsigned char* IV, size_t offset_out)
+size_t ske_encrypt_file(const char *fnout, const char *fnin,
+						SKE_KEY *K, unsigned char *IV, size_t offset_out)
 {
 	/* TODO: write this.  Hint: mmap. */
 	return 0;
 }
-size_t ske_decrypt(unsigned char* outBuf, unsigned char* inBuf, size_t len,
-		SKE_KEY* K)
+size_t ske_decrypt(unsigned char *outBuf, unsigned char *inBuf, size_t len,
+				   SKE_KEY *K)
 {
 	/* TODO: write this.  Make sure you check the mac before decypting!
 	 * Oh, and also, return -1 if the ciphertext is found invalid.
 	 * Otherwise, return the number of bytes written.  See aes-example.c
 	 * for how to do basic decryption. */
-	return 0;
+	// extract IV
+	unsigned char IV[16];
+	memcpy(IV, inBuf, 16);
+	// extract ciphertext
+	unsigned int ciphertextLength = len - 16 - 32;
+	unsigned char c[ciphertextLength];
+	memcpy(c, inBuf + 16, ciphertextLength);
+	// extract hmac
+	unsigned char hmac[32];
+	memcpy(hmac, inBuf + ciphertextLength + 16, 32);
+	// check if the mac is valid
+	unsigned char *checkhmac = malloc(32);
+	unsigned char IVC[len - 32];
+	memcpy(IVC, inBuf, len - 32);
+	HMAC(EVP_sha256(), K->hmacKey, 32, IVC, len - 32, checkhmac, NULL);
+	if (memcmp(hmac, checkhmac, 32) != 0)
+	{
+		free(checkhmac);
+		return -1;
+	}
+	// perform the decryption
+	EVP_CIPHER_CTX *ciphertext = EVP_CIPHER_CTX_new();
+	EVP_DecryptInit_ex(ciphertext, EVP_aes_256_ctr(), 0, K->aesKey, IV);
+	int bytesWritten;
+	EVP_DecryptUpdate(ciphertext, outBuf, &bytesWritten, c, ciphertextLength);
+	// free up memory
+	EVP_CIPHER_CTX_free(ciphertext);
+	free(checkhmac);
+	return bytesWritten;
 }
-size_t ske_decrypt_file(const char* fnout, const char* fnin,
-		SKE_KEY* K, size_t offset_in)
+size_t ske_decrypt_file(const char *fnout, const char *fnin,
+						SKE_KEY *K, size_t offset_in)
 {
 	/* TODO: write this. */
 	return 0;
